5f792e8eb0b70177280f04c3e05e4b9105694f65
Joseph P <jpark041@gmail.com>
2019-07-04 02:59:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age:"54"},
-      :hero =>      {name: "Romeo",age: "15", status: "alive"},
+      :hero =>      {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    
