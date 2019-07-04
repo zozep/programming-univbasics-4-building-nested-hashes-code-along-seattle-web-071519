618075b7645d06a948e829477d61432c0125f304
Joseph P <jpark041@gmail.com>
2019-07-04 02:53:21 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -11,7 +11,7 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :hero_friends => []
       }}
 
